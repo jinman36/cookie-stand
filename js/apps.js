c2a54449091edd5662327f3884f562680fd67381
Jeff Inman <jeffery.inman@hotmail.com>
2021-02-01 21:36:17 -0800

    added css and played with code

--- js/apps.js
+++ js/apps.js
@@ -4,6 +4,7 @@ let cookieTable = document.getElementById('table');
 let tableHeader = document.getElementById('header');
 let tableBody = document.getElementById('body');
 let tableFooter = document.getElementById('footer');
+
 let totalCookieStands = [];
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
