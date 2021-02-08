f2ec9c6ad772b3e72140107a891a4d5f612a5d98
Jeff Inman <jeffery.inman@hotmail.com>
2021-02-07 20:23:47 -0800

    added stuff

--- js/apps.js
+++ js/apps.js
@@ -7,7 +7,7 @@ let tableFooter = document.getElementById('footer');
 
 // Add submission form to sales page
 let myForm = document.getElementById('container');
-let parentEl = document.getElementById('results');
+// let parentEl = document.getElementById('results');
 
 let totalCookieStands = [];
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
